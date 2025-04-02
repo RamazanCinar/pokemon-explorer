d5b1ccd82a54f5a0bea40cf81deb31be52695999
ramazan <ramazancinar0858@gmail.com>
2025-04-02 17:22:49 +0200

    add PokemonCard component

---
+++ src/app/components/PokemonCard.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export interface PokemonCardProps {
+  id: number;
+  name: string;
+  image: string;
+  type: string;
+}
+
+const PokemonCard: React.FC<PokemonCardProps> = ({ id, name, image, type }) => {
+  return (
+    <div>
+      <Image src={image} alt={name} />
+      <p>#{id}</p>
+      <h2>{name}</h2>
+      <p>{type}</p>
+    </div>
+  );
+};
+
+export default PokemonCard;
