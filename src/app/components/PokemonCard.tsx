598bd04aab1564e79416a83200d0def69e03daf5
ramazan <ramazancinar0858@gmail.com>
2025-04-03 00:51:14 +0200

    add favorite button to PokemonCard

--- src/app/components/PokemonCard.tsx
+++ src/app/components/PokemonCard.tsx
@@ -5,15 +5,27 @@ export interface PokemonCardProps {
   name: string;
   image: string;
   type: string;
+  isFavorite: boolean;
+  toggleFavorite: () => void;
 }
 
-const PokemonCard: React.FC<PokemonCardProps> = ({ id, name, image, type }) => {
+const PokemonCard: React.FC<PokemonCardProps> = ({
+  id,
+  name,
+  image,
+  type,
+  isFavorite,
+  toggleFavorite,
+}) => {
   return (
     <div>
       <Image src={image} alt={name} width={100} height={100} />
       <p>#{id}</p>
       <h2>{name}</h2>
       <p>{type}</p>
+      <button onClick={toggleFavorite}>
+        {isFavorite ? "Remove Favorite" : "Add Favorite"}
+      </button>
     </div>
   );
 };
